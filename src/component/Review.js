545d015bdc9d202df03624fd7f667a302d0dccf3
alok01895 <alok01895@gmail.com>
2022-12-23 16:52:12 +0530

    made changes in review page,deleted a page

--- src/component/Review.js
+++ src/component/Review.js
@@ -3,7 +3,16 @@ import React from 'react'
 export default function Review() {
   return (
     <div>
-      review
+      <div className="comment" >
+        <div className="com">
+        <h3>Write a comment...</h3>
+        <div className='comment-box'>
+          <input type="text" className='com-section' name="comm"  placeholder='Enter....' /> 
+          <button id='post'>Post</button>
+        </div>
+        </div>
+      </div>
+
     </div>
   )
 }
